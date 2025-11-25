aa1d04849b4163f99c0c76f48492f806b9a73701
TanguyLH <tanguy.tlh@gmail.com>
2025-11-25 16:52:18 +0100

    Now printing the map

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "json.hpp"
 
 using json = nlohmann::json;
 
+bool validateJson(json mapData);
+
 int main() {
+    
+    std::map<int, char> mapAsciiTranslator = {
+        { 0, '.'},
+        { 1, '#'},
+        { 2, '~'},
+    };
+
     std::ifstream file("../maps/base0.json");
     if(!file.is_open()) {
         std::cerr << "Error: unable to open the file" << std::endl;
@@ -15,12 +25,28 @@ int main() {
     file >> mapData;
     file.close();
     
-    std::cout << "Parsed map:" << std::endl;
-    std::cout << mapData.dump(4) << std::endl;
+    validateJson(mapData);
     
-    if (mapData.contains("width")) {
-        std::cout << "\nWidth:" << mapData["width"] << std::endl;
+    for (int y = 0; y < mapData["height"]; y++) {
+        for (int x = 0; x < mapData["width"]; x++) {
+            std::cout << mapData["tiles"][y][x] << " ";
+        }
+        std::cout << std::endl;
     }
 
     return 0;
+}
+
+bool validateJson(json mapData) {
+    if (!mapData.contains("width")) {
+        std::cout << "Width not found" << std::endl;
+        return false;
+    }
+
+    if (!mapData.contains("width")) {
+        std::cout << "Width not found" << std::endl;
+        return false;
+    }
+    
+    return true;
 }
